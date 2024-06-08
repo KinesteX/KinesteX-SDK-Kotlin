39f96bc981aa3fcdca5a8d57238e1f841b7cbe0c
Vladimir Shetnikov <vladimir@kinestex.com>
2024-06-08 17:16:17 +0400

    added new integration options

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
 dependencies {
 
-    implementation(project(":kinestexsdkkotlin"))
+    //implementation(project(":kinestexsdkkotlin"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
