1463036cc55e467dce8dd0833a9460f9e4164b34
Vladimir Shetnikov <vladimir@kinestex.com>
2024-06-08 18:46:18 +0400

    changed version

--- kinestexsdkkotlin/build.gradle.kts
+++ kinestexsdkkotlin/build.gradle.kts
@@ -39,7 +39,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.KinesteX"
                 artifactId = "kinestexsdkkotlin"
-                version = "1.0.2"
+                version = "1.1.0"
 
                 pom {
                     name.set("KinesteX SDK Kotlin")
